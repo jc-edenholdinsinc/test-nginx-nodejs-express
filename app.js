9c84573fcaa63da586b59f6f3b357e7f2415f6e1
JC Santos <jc@motley.works>
2022-05-25 23:33:22 +0800

    Updated environment variables

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 
 require("dotenv").config()
 const client = require('@mailchimp/mailchimp_marketing');
+const md5 = require('md5')
+
 client.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
   server: process.env.MAILCHIMP_SERVER_PREFIX,
 });
 
 const run = async () => {
+//   const response = await client.lists.getListMember(process.env.MAILCHIMP_AUDIENCE_ID, md5("jcsantosdevtest6@gmail.com"));
+//   console.log(response)
   const response = await client.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
     email_address: "jcsantosdevtest6@gmail.com",
     status: "subscribed",
   });
 
-  if (response.errors.length) {
-    throw new Error(response.errors);
-  } else {
-      console.log("no issue!");
-  }
-
+//   if (response.errors.length) {
+//         throw new Error(response.errors);
+//   } else {
+//         console.log("no issue!");
+//   }
+//   console.log(response.hasOwnProperty("unique_email_id"))
 //   console.log(JSON.stringify(response))
 //   console.log(searchParams.get('status'));
+
+  if (!response.hasOwnProperty("unique_email_id")) {
+      console.log(response.body)
+  } else {
+      console.log("successfully added!");
+  }
 };
 
-run().catch(errors => console.log(errors));
+run();
 
 
 // const express = require("express");
